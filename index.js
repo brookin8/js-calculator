2cc24a496d3aa88a5606c41fe8e72411776e7cb1
Erin Brookins Wilcox <brookin8@gmail.com>
2017-09-28 15:26:05 -0400

    Fixed port issue with express

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/app/'));
+
+//app.get('/',function(req,res) {
+//	res.send('Hello World!')
+//})
 
 app.listen(port, function() {
   console.log('I am listening on port 3000!');
